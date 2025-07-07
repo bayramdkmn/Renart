7179d06b34cdb28388545a8853d3e3302b01b349
Bayram Dikmen <bayramdikmenn@gmail.com>
2025-07-07 18:15:12 +0300

    url edited

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const fetchProducts = async () => {
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:3001/api/products");
+      const response = await fetch("https://renart-backend.vercel.app");
       if (!response.ok) {
         throw new Error("Ürünler yüklenemedi");
       }
